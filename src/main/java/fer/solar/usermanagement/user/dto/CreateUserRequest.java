49295e2f38e6f26bd4be0fbf943a9d7b56d39faa
BernardKazazic <bernard.kazazic@gmail.com>
2025-04-11 00:26:25 +0200

    pr fix 2

--- src/main/java/fer/solar/usermanagement/user/dto/CreateUserRequest.java
+++ src/main/java/fer/solar/usermanagement/user/dto/CreateUserRequest.java
@@ -10,6 +10,5 @@ public class CreateUserRequest {
     private String connection; // e.g., "Username-Password-Authentication"
     private List<String> roleIds; // List of Auth0 Role IDs to assign
     private String resultUrl; // Optional: URL to redirect user after password change
-    // Add other relevant fields if needed, like name, etc.
     // Auth0 invitation will handle the password setup
 } 
